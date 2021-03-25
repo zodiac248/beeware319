911d25d2fe98965d5cd9fce77601e726aaba3a7b
zodiac248 <roy971128@gmail.com>
2021-03-25 17:23:22 +0900

    fix origin

--- src/main/java/com/panpal/security/SecurityConfig.java
+++ src/main/java/com/panpal/security/SecurityConfig.java
@@ -54,6 +54,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll();
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .oidcUserService(oidcUserService);
     }
 }
