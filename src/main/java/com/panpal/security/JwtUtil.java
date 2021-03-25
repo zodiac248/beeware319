4ee9657c7e2119076c34fa980d84f1c670d9c99d
zodiac248 <roy971128@gmail.com>
2021-03-26 00:05:48 +0900

    fix origin

---
+++ src/main/java/com/panpal/security/JwtUtil.java
@@ -0,0 +1,40 @@
+package com.panpal.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.panpal.User.User;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtUtil {
+	private static final String secret = "this is a secret";
+
+    public static String generateToken(User user) {
+        Map<String, Object> claims = new HashMap<>();
+        try{
+            return JWT.create().withClaim("id", user.getId()).withClaim("name",user.getName())
+                    .sign(Algorithm.HMAC256(secret));
+        }catch (Exception e){
+            return  null;
+        }
+    }
+
+    public static User parseToken(String token) {
+ //       try {
+ //           JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(secret))
+ //                   .build();/ //            DecodedJWT decodeToken = jwtVerifier.verify(token);
+ //           User user = new User();
+ //           user.setId(decodeToken.getClaim("id").asInt());
+ //           user.setName(decodeToken.getClaim("name").asString());
+ //           return user;
+ //       }
+ //       catch (UnsupportedEncodingException e) {
+ //           return null;
+ //       }
+        return null;
+    }
+ }
---
+++ src/main/java/com/panpal/security/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.panpal.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.annotation.Resource;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Resource
+//    private AccessDeniedHandler accessDeniedHandler;
+//    @Resource
+//    private JwtFilter jwtFilter;
+//
+//    @Override
+//    protected void configure(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.cors().and().csrf().disable()
+//                .exceptionHandling().accessDeniedHandler(accessDeniedHandler).and()
+//
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+//
+//                .authorizeRequests()
+//
+//                .anyRequest().authenticated();
+//
+//        httpSecurity.addFilterAt(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+//    }
+//
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.POST, "/users/login");
+//        web.ignoring().antMatchers(HttpMethod.GET, "/users/userinfo");
+    }
+
+
+    private final OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService;
+
+    public SecurityConfig(OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService) {
+        this.oidcUserService = oidcUserService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .oidcUserService(oidcUserService);
+    }
+}
