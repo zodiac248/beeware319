e1e20a4fa9ae12b293e57203d9c1bf2448acff2a
zodiac248 <roy971128@gmail.com>
2021-04-07 01:58:32 +0900

    777

---
+++ src/main/java/com/panpal/security/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.panpal.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.annotation.Resource;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Resource
+    private AccessDeniedHandler accessDeniedHandler;
+
+    @Resource
+    private JwtFilter jwtFilter;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors().and().csrf().disable()
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler).and()
+
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+
+                .authorizeRequests()
+
+                .anyRequest().authenticated();
+
+        httpSecurity.addFilterAt(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+//
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.POST, "/users/login");
+        web.ignoring().antMatchers(HttpMethod.GET, "/user/search");
+        web.ignoring().antMatchers(HttpMethod.GET, "/api/user/search");
+
+    }
+
+}
