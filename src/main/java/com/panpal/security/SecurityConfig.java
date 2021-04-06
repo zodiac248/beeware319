42626fe1837be614a124bd54814db1b055d34fa9
zodiac248 <roy971128@gmail.com>
2021-04-07 03:05:02 +0900

    777

--- src/main/java/com/panpal/security/SecurityConfig.java
+++ src/main/java/com/panpal/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.panpal.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,9 +13,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @EnableWebSecurity
@@ -26,12 +33,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Resource
     private JwtFilter jwtFilter;
 
+
     @Resource
     private SimpleCORSFilter simpleCORSFilter;
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable()
+        httpSecurity.cors().configurationSource(CorsConfigurationSource()).and().csrf().disable()
                 .exceptionHandling().accessDeniedHandler(accessDeniedHandler).and()
 
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
@@ -39,10 +47,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
 
                 .anyRequest().authenticated().requestMatchers(CorsUtils::isPreFlightRequest).permitAll().antMatchers(HttpMethod.OPTIONS)
-                .permitAll().and().addFilterBefore(simpleCORSFilter, ChannelProcessingFilter.class);
+                .permitAll();
         httpSecurity.addFilterAt(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
-//
+
+    //
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers(HttpMethod.POST, "/users/login");
@@ -51,4 +60,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
+    private CorsConfigurationSource CorsConfigurationSource() {
+        CorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");    //同源配置，*表示任何请求都视为同源，若需指定ip和端口可以改为如“localhost：8080”，多个以“，”分隔；
+        corsConfiguration.addAllowedHeader("*");//header，允许哪些header，本案中使用的是token，此处可将*替换为token；
+        corsConfiguration.addAllowedMethod("*");    //允许的请求方法，PSOT、GET等
+        ((UrlBasedCorsConfigurationSource) source).registerCorsConfiguration("/**", corsConfiguration); //配置允许跨域访问的url
+        return source;
+    }
 }
+
+
+
+
